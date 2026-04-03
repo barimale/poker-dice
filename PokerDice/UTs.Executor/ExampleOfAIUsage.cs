137b528af1d2fcf5c76cdf4098ee52e107a70a2b
Mateusz <mateusz.wolnica@gmail.com>
2026-04-03 15:06:15 +0200

    .

--- PokerDice/UTs.Executor/ExampleOfAIUsage.cs
+++ PokerDice/UTs.Executor/ExampleOfAIUsage.cs
@@ -7,13 +7,11 @@ namespace UTs.Executor
 {
     public class ExampleOfAIUsage: PrintToConsoleUTBase
     {
-        private readonly TextWriter _originalOut;
         private readonly TestOutputTextWriter _redirectWriter;
 
         public ExampleOfAIUsage(ITestOutputHelper output)
             : base(output)
         {
-            _originalOut = Console.Out;
             _redirectWriter = new TestOutputTextWriter(output);
             Console.SetOut(_redirectWriter);
         }
